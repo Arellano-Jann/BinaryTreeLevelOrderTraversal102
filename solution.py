2198835e0750eb47d420c3f549f5d13fb364b550
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-08-02 18:50:46 -0700

    added recursive solutions

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ class ListNode:
 
 def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
     reversed1 = None
-    while head != None:
+    while head:
         current = head.next # creates a new ListNode object with all elements but the first element of the list
         head.next = reversed1 # isolates the first element of the List by replacing the next variable with the current reversed list (starting with None)
         reversed1 = head # saves the current head to the current reversed list (because it's currently reversed lol)
@@ -14,4 +14,20 @@ def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
     return reversed1
 
 
-# recursive solution
+# recursive solution. i actually have no idea how this works bc i refuse to draw it out. but here anyway lol
+def reverseList2( head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next: return head # this is not head and not head.next so we can rearrange head.next.next to be the correct head. checks if it's the end of the list. not head is needed here because of empty head input.
+    node = reverseList2(head.next) # reversed list
+    print(head.next)
+    print(node)
+    head.next.next = head # makes a circle to point it to itself
+    print(head)
+    head.next = None # isolates?
+    return node
+
+# recursive solution but muy slow.
+def reverseList3(head: Optional[ListNode], newHead = None) -> Optional[ListNode]:
+    if not head: return newHead # returns the full reversed list
+    next = head.next # creates a new ListNode featuring all of List[1:]
+    head.next = newHead # isolates current head and replaces the end with the current reversed list 
+    return reverseList3(next, head)
